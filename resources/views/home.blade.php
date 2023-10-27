a2b484719da09d7913235dfac35efeceb5364154
matteoNapoli42 <matteon4242@gmail.com>
2023-10-27 17:12:29 +0200

    starting with styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laravel Model Controller</title>
+    @vite('resources/js/app.js')
 </head>
 
 <body>
-    @foreach($movies as $movie)
-    <span>{{$movie['title']}}</span>
-    @endforeach
+    <h1>Movies</h1>
+    <div class="myContainer ">
+        @foreach($movies as $movie)
+        <div class="card">
+            <h4>{{$movie['title']}}</h4>
+            <span>Data di rilascio : {{$movie['date']}}</span>
+            <span>Nazione : {{$movie['nationality']}}</span>
+            <span>Voto : {{$movie['vote']}}</span>
+        </div>
+        @endforeach
+    </div>
+
+
+
+
 </body>
 
 </html>
